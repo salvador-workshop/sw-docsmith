b2dba3a36a0a0a0966b99ff06efb49fef07b8eff
RJ Salvador <salvador.workshop@gmail.com>
2026-05-03 15:27:42 -0400

    Picsmith functions now being invoked

--- src/sw_docsmith/process_image.py
+++ src/sw_docsmith/process_image.py
@@ -1,6 +1,12 @@
 import argparse
 import os
 
+from .picsmith.transparentize import transparentize
+from .picsmith.transparentize_desat import transparentize_desat
+from .picsmith.engraving import engraving
+from .picsmith.black_to_colour import black_to_colour
+from .picsmith.pull_ebook_images import pull_ebook_images
+
 root_dir = os.path.join(os.path.dirname(__file__), "..", "..")
 input_dir = os.path.join(root_dir, "input")
 output_dir = os.path.join(root_dir, "output")
@@ -17,21 +23,35 @@ def process_image():
         "[ 4 ] -- Convert B&W Images to Two Colours",
         "[ 5 ] -- Copy E-Book Images",
     ]
-    mode_help_msg =  "Available op. modes:  " + ", ".join(mode_help_msgs)
+    mode_help_msg = "Available op. modes:  " + ", ".join(mode_help_msgs)
     parser.add_argument(
         "--mode",
         dest="mode",
-        default=None,
+        default=0,
         help=mode_help_msg,
     )
 
     args = parser.parse_args()
-    input_mode = args.mode
+    input_mode = int(args.mode)
 
     print("\n----------------------------------------------------------------\n")
 
-    print(f"> Test content")
-    print(f"> Image Processing Mode: {input_mode}")
+    print(f"> PROCESS IMAGE")
+    print("\n----------------------------------------------------------------\n")
+    print(f"> [Image Processing Mode] {input_mode}")
+
+    if input_mode == 1:
+        transparentize()
+    elif input_mode == 2:
+        transparentize_desat()
+    elif input_mode == 3:
+        engraving()
+    elif input_mode == 4:
+        black_to_colour()
+    elif input_mode == 5:
+        pull_ebook_images()
+    else:
+        print("> [Warning] Operation mode not recognized. No action taken.")
 
     print("\n----------------------------------------------------------------\n")
 
